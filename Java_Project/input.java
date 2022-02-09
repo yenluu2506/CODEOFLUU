8d90a724dbaa5eefe1b87b878b4e0a4eb05e1dc2
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2022-02-09 15:51:17 +0700

    Add  Java_Project/ file

---
+++ Java_Project/input.java
@@ -0,0 +1,10 @@
+package Java_Project;
+import java.util.Scanner;
+
+public class input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello " + name);
+    }
+}
---
+++ Java_Project/op_kitudungtruoc.java
@@ -0,0 +1,11 @@
+package Java_Project;
+import java.util.Scanner;
+
+
+public class op_kitudungtruoc {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = (char)(sc.next().charAt(0)+1);
+		System.out.println(c);
+	}
+}
