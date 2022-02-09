8d297056bee01fe74f80e6d617981404f5702d31
Bui Thi Yen Luu <bachhoangthienduy166@gmail.com>
2022-02-09 21:06:34 +0700

    Add Java_Project/ file

---
+++ Java_Project/bool_goc_phan4.java
@@ -0,0 +1,23 @@
+package Java_Project;
+
+import java.util.Scanner;
+
+public class bool_goc_phan4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        if(x>0&&y>0){
+            System.out.println("This point lies in the First quadrant");
+        }
+        else if(x>0&&y<0){
+            System.out.println("This point lies in the Fourth quadrant");
+        }
+        else if(x<0&&y>0){
+            System.out.println("This point lies in the Second quadrant");
+        }
+        else {
+            System.out.println("This point lies in the Third quadrant");
+        }
+    }
+}
---
+++ Java_Project/bool_in_decreasing.java
@@ -0,0 +1,21 @@
+package Java_Project;
+
+import java.util.Scanner;
+
+public class bool_in_decreasing {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a<=b && b<=c){
+			System.out.println("increasing");
+		}
+		else if(a>=b && b>=c){
+			System.out.println("decreasing");
+		}
+		else {
+			System.out.println("neither increasing nor decreasing order");
+		}
+	}
+}
